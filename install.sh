1f49b8169030848c3c1cf69c4f8b92c683e186e6
Toby Baldock <toby.baldock@sky.uk>
2024-07-24 19:25:40 +0100

    Bug #1: fixed a couple of typos

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ BIN_DIR="${MY_DIR}/bin"
 
 echo "Please run the following commands:" 1>&2
 echo ""
-echo "pip install -r ${MY_DIR}/requirement.txt"
+echo "pip install -r ${MY_DIR}/requirements.txt"
 echo "export PATH=\"${BIN_DIR}:\$PATH\""
 echo ""
 echo "To permanently install this, add the export command as a line in your ~/.bashrc file" 1>&2
